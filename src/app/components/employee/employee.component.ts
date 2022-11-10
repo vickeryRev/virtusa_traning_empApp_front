37ab968cf19520642007f82a78ffe17534ebfa72
vickeryRev <vickery20091991@gmail.com>
2022-11-10 12:17:08 -0600

    fixed issues where front end was fireing blank data into backend, kinda

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -13,44 +13,44 @@ export class EmployeeComponent implements OnInit {
   title = "Add Employee"
 
   dep: string = "";
+  fn: string = "";
+  ln: string = "";
+  em: string = "";
   emp: Employee = new Employee(0, '', '', '', '');
   added: any = "";
 
   constructor(private EmployeeService: EmployeeService, private FormBuilder: FormBuilder) { }
   empForm: FormGroup = new FormGroup({
-    firstName: new FormControl('', [
+    firstName: new FormControl(this.emp.firstName, [
       Validators.required,
       Validators.pattern(`^[A-Za-z]+$`)
     ]),
 
-    lastName: new FormControl('', [
+    lastName: new FormControl(this.emp.lastName, [
       Validators.required,
       Validators.pattern(`^[A-Za-z]+$`)
     ]),
-    department: new FormControl('', [Validators.required]),
-    email: new FormControl('', [
+    department: new FormControl(this.emp.department, [Validators.required]),
+    email: new FormControl(this.emp.email, [
+      Validators.required,
 
     ])
 
   })
 
-  /*
-  changeDept(e: any) {
-    this.department?.setValue(e.target.value), {
-      onlySelf: true,
-    }
+
+
+
+
+  save(data: any): void {
+    this.emp.firstName = data.firstName;
   }
 
-  get department(): any {
-
-    return this.empForm.get('department');
-
-  }
-  */
-
-
-  addEmployee(): void {
-    this.emp.department = this.dep;
+  addEmployee(data: any): void {
+    this.emp.firstName = data.firstName;
+    this.emp.lastName = data.lastName;
+    this.emp.department = data.department;
+    this.emp.email = data.email;
     this.EmployeeService.addEmployee(this.emp).subscribe(
       data => this.added = `Added ${data.firstName}`
     )
